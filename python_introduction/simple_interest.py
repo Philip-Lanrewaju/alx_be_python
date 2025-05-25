26cb45a067011ef649336d72a1818ef8c6e9eb24
Philip Olanrewaju <biodunlanrewaju@gmail.com>
2025-05-25 15:41:05 +0100

    Add simple_interest.py to calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,15 @@
+#Principal is $ 1,000 represented by P
+P = 1000 
+
+#At 5% rate per annum represented by R
+R = 0.05  
+
+# For a 3 years period, represented by T
+T = 3
+
+
+# The simple interest (represented by I) on the principal in three years at 5% is:
+
+I = P*R*T
+
+print(f"The simple interest is: {I}")
