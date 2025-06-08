23f0d8edb2901f175b3e2f8771af382be07912b4
Philip Olanrewaju <biodunlanrewaju@gmail.com>
2025-06-08 16:53:08 +0100

    Add arithmetic_operations.py as a function to perform arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,6 @@ def perform_operation(num1, num2, operation):
     elif operation == "divide":
         if num2 == 0:
             return "Error! You cannot divide by 0"
-        return num1/num2   
+        return num1/num2
     else:
         return "Invalid Operation Selected"
